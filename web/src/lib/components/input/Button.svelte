08ea67c007ec87326c46351e668cca98d967e982
bailey <spiritov_v@pm.me>
2026-03-24 17:44:15 -0700

    update button styling

--- web/src/lib/components/input/Button.svelte
+++ web/src/lib/components/input/Button.svelte
@@ -3,18 +3,20 @@
 
   type Props = {
     table?: boolean;
+    grow?: boolean;
     children: Snippet;
     onsubmit: () => Promise<boolean>;
   };
 
-  let { children, table = false, onsubmit }: Props = $props();
+  let { children, table = false, grow = false, onsubmit }: Props = $props();
 
   let valid: Promise<boolean> = $state(Promise.resolve(true));
 </script>
 
 <button
-  class="relative flex h-9 max-w-fit cursor-pointer items-center justify-between gap-1 rounded-box border-base-700 bg-base-800 px-2 text-nowrap transition-colors hover:border-content/50 hover:bg-base-900
-  {table ? 'border' : 'min-w-40 border border-b-content/50'}"
+  class="relative flex h-9 cursor-pointer items-center gap-1 rounded-box border-base-700 bg-base-800 px-2 text-nowrap transition-colors hover:border-content/50 hover:bg-base-900
+  {table ? 'border' : 'min-w-40 border border-b-content/50'}
+  {grow ? 'w-full justify-center' : 'max-w-fit justify-between'}"
   onclick={() => {
     valid = onsubmit();
   }}>
